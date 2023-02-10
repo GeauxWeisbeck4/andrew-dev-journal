19931ebe41c5e199c3c5df7aefb9f5f622e550ec
Andrew Weisbeck <andrew.weisbeck@icloud.com>
2023-02-09 23:57:04 -0500

    Initial commit

---
+++ data/headerNavLinks.js
@@ -0,0 +1,23 @@
+const headerNavLinks = [
+  {
+    href: '/about',
+    title: 'About',
+  },
+  {
+    href: '/tags',
+    title: 'Tags',
+  },
+  {
+    href: '/projects',
+    title: 'Projects',
+  },
+  {
+    href: '/blog',
+    title: 'Blog',
+  },
+  {
+    href: '/resources',
+    title: 'Resources',
+  }
+]
+export default headerNavLinks
---
+++ data/projectsData.js
@@ -0,0 +1,21 @@
+const projectsData = [
+  {
+    title: 'GeauxWeisbeck4.dev',
+    description: `It once served as my personal website, but it now serves as my home for freelancing. It sort of still resembles a personal site, I just try to keep it more professional while I have more personal things on AndrewWeisbeck.dev. `,
+    imgSrc: '/static/images/google.png',
+    href: 'https://geauxweisbeck4.dev',
+  },
+  {
+    title: 'My Personal Blog Created With Deno',
+    description: `This is my personal Deno blog that I created from a popular template on Deno's main website. Initially, I just wanted to see how Deno Deploy worked and wanted a cool and easy project for my Deno Deploy account. Now I am actually updating it and planning to further expand the site.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Geaux Random Quote Generator',
+    description: 'This simple project was done as a project just for fun - I thought about implementing it into some kind of cool app that people could use, but then I didnt.',
+    imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://geauxrandomquote.netlify.app',
+  }
+]
+export default projectsData
